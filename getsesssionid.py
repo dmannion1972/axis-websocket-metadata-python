73151d20279d72c987c54c3d03b733d1564ccad6
Don Mannion <115176215+dmannion1972@users.noreply.github.com>
2023-03-15 11:10:48 -0400

    Update getsesssionid.py

--- getsesssionid.py
+++ getsesssionid.py
@@ -1,15 +1,24 @@
-
 import requests
 import json
 from requests.auth import HTTPDigestAuth 
-cam_user = 'root'
-cam_pass = 'pass'
+print('Enter your IP address, example 192.168.0.90')
+ip = input()
+ip_address = ip
+print ("Username for Axis product ")
+axis_user = input()
+axis_username = axis_user
+print("Enter password of Axis product ")
+axis_pass = input()
+axis_pw = axis_pass
+
+cam_user = axis_username
+cam_pass = axis_pw
 auth=HTTPDigestAuth(cam_user, cam_pass)
 
 # before you can create a websocket connection we must request a token.  
-urlsessionid= 'http://192.168.20.195/axis-cgi/wssession.cgi'
+urlsessionid= 'http://'+ip_address+'/axis-cgi/wssession.cgi'
 
 response = requests.request("POST", urlsessionid, auth=auth)
 sessionID_responseJsonDict = (response.text)
+print(sessionID_responseJsonDict)
 token = sessionID_responseJsonDict
-
