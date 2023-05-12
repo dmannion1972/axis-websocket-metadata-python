ab9e088552e0b259ba02ea32e81737ed5919c4a4
Don Mannion <115176215+dmannion1972@users.noreply.github.com>
2023-05-12 02:53:02 -0400

    Update getsesssionid.py
    changed password using getpass

--- getsesssionid.py
+++ getsesssionid.py
@@ -1,24 +1,16 @@
 import requests
-import json
 from requests.auth import HTTPDigestAuth 
-print('Enter your IP address, example 192.168.0.90')
-ip = input()
-ip_address = ip
-print ("Username for Axis product ")
-axis_user = input()
-axis_username = axis_user
-print("Enter password of Axis product ")
-axis_pass = input()
-axis_pw = axis_pass
+from getpass import getpass
 
-cam_user = axis_username
-cam_pass = axis_pw
-auth=HTTPDigestAuth(cam_user, cam_pass)
+ip_address = input("Enter your IP address: ")
+cam_user = input("Enter username: ")
+cam_pass = getpass("Enter password: ")
+auth = HTTPDigestAuth(cam_user, cam_pass)
 
-# before you can create a websocket connection we must request a token.  
-urlsessionid= 'http://'+ip_address+'/axis-cgi/wssession.cgi'
+urlsessionid= f'http://{ip_address}/axis-cgi/wssession.cgi'
 
 response = requests.request("POST", urlsessionid, auth=auth)
 sessionID_responseJsonDict = (response.text)
+print(sessionID_responseJsonDict)
 token = sessionID_responseJsonDict
 
